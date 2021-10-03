ca9c4e2225ad8312f9d035c881a21cbfa6eba188
Maca <66545374+macareen@users.noreply.github.com>
2021-10-03 19:02:23 -0300

    mios

--- src/lightgbm/682_lgb_prob_auto_m.R
+++ src/lightgbm/682_lgb_prob_auto_m.R
@@ -28,7 +28,7 @@ require("mlrMBO")
 #para poder usarlo en la PC y en la nube sin tener que cambiar la ruta
 #cambiar aqui las rutas en su maquina
 switch ( Sys.info()[['sysname']],
-         Windows = { directory.root  <-  "M:\\" },   #Windows
+         Windows = { directory.root  <-  "C:/Users/macar/Documents/Educacion/Maestria_DM/2021_2C/DMEF" },   #Windows
          Darwin  = { directory.root  <-  "~/dm/" },  #Apple MAC
          Linux   = { directory.root  <-  "~/buckets/b1/" } #Google Cloud
        )
